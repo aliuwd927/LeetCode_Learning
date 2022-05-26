339a264ea92efd92cc2c88a0833fd349d6c10781
Andy Liu <aliu927@gmail.com>
2022-05-25 22:02:26 -0700

    Learning Singly Linked List

---
+++ Array and Strings/Single Linked List/Singly Linked List.js
@@ -0,0 +1,14 @@
+class MakeNode {
+  constructor(data) {
+    this.data = null;
+    this.next = null;
+  }
+}
+
+class List {
+  constructor() {
+    this.start = null;
+    this.end = null;
+    this.length = 0;
+  }
+}
